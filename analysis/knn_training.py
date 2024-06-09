d75771d7f750021e05616c43fcc178e1ed5ef4c4
DJ200148 <81722638+DJ200148@users.noreply.github.com>
2024-06-09 10:37:34 -0700

    feature selection update

--- analysis/knn_training.py
+++ analysis/knn_training.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn.neighbors import KNeighborsClassifier
 from dtaidistance import dtw
 import joblib
